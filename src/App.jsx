808aa03b392ffe1a7ce91411cf24254ea33e1585
Vaishal Parikh <vaishalparikh@MacBook-Air.local>
2025-01-04 12:47:33 +0530

    Fixed shadcn style issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { Button } from './components/ui/button'
 import './App.css'
+import { Input } from './components/ui/input'
 
 function App() {
 
+
   return (
-    <>
-    <div className='text-2xl'>
-      Hey Vaishal!
+    <div>
+      <Button variant= "default">Click me</Button>
+      <Button variant="outline">Button</Button>
+      <Button variant="link">Button</Button>
+      <h1 className="text-4xl font-bold text-center">
+        Hello, Vite + React!
+      </h1>
+      <Input placeholder="Enter your name" />
     </div>
-    </>
   )
 }
 
